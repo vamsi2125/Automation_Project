bd212c081272e7d49a53ddb0ce5da632e5efd457
vamsi2125 <vamsimanohar25@gmail.com>
2022-02-16 01:33:48 +0530

    added cron job script

--- automation.sh
+++ automation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 name="vamsi"
-s3bucket="upgrad-vamsi/logs"
+s3bucket="upgrad-vamsi"
 
 sudo apt update -y
 sudo apt install apache2 -y
@@ -32,3 +32,20 @@ sudo apt-get install awscli -y
 aws s3 \
 cp /tmp/${name}-httpd-logs-${timestamp}.tar \
 s3://${s3bucket}/${name}-httpd-logs-${timestamp}.tar
+
+if [ -e /var/www/html/inventory.html ]
+then
+        echo "Inventory exists"
+else
+        touch /var/www/html/inventory.html
+        echo "<b>Log Type &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Date Created &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Type &nbsp;&nbsp;$
+fi
+echo "<br>httpd-logs &nbsp;&nbsp;&nbsp;&nbsp; ${timestamp} &nbsp;&nbsp;&nbsp;&nbsp; tar &nbsp;&nbsp;&nbsp;&nbsp; `du -h $
+if [ -e /etc/cron.d/automation ]
+then
+        echo "Cron job exists"
+else
+        touch /etc/cron.d/automation
+        echo "0 0 * * * root /root/Automation_Project/automation.sh" > /etc/cron.d/automation
+        echo "Cron job added"
+fi
